1ade0ba52082658fa64267bff77fc99c1a857102
JawadCyberspace <56886536+CyberSpaceJawadMalik@users.noreply.github.com>
2022-09-02 15:39:13 +0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
 export const CONFIG = {
   TEMPLATE: {
     // 1. Set your NFT collection contract address
-    "collection_address": "0x9498274b8c82b4a3127d67839f2127f2ae9753f4",
+    "collection_address": "0x5180db8F5c931aaE63c74266b211F580155ecac8",
 
     // 2. Set your blockchain chain ID where your NFT collection contract address is deployed (see below for value options)
-    "block_chain_id": "137",
+    "block_chain_id": "1",
 
     // 3. Set the default title of your dashboard. If found, this template uses the NFT Collection name for the title.
-    "title": "My NFT Collection",
+    "title": "My Cryptocorvet Dashboard",
 
     // 4. Set your Covalent API Key
     "api_key": "ckey_3bdabb7325ee47cc9bcc1e5f3d7",
@@ -16,7 +16,7 @@ export const CONFIG = {
     "timeseries_chart": true,
 
     // 6. (Optional) Set your banner image
-    "banner_picture": "",
+    "banner_picture": "https://lh3.googleusercontent.com/K6KUPNbTYFxVJ5_0Y9gOlZ6UMqwM7EsRBmPKhtv03nvJpc0lvPIEPNJbth8gkAEEuVaDqLx4gAo141F2Qh4HJOqpfbODK7GXiG53jw8=w600",
   },
 
   // Supported networks - just for your reference
